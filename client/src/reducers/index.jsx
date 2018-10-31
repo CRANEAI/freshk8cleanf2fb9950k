53912c0060d91f0a9e7899dd8b7c617813aeb7c9
Elly <elly@mg.crane.ai>
2018-10-31 00:12:49 -0400

    added client/src/reducers/index refactoring

---
+++ client/src/reducers/index.jsx
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux"
+import { reducer as Form } from "redux-form"
+
+import get_started from "./reducer_get_started"
+import get_started from "./reducer_get_started"
+import get_started from "./reducer_get_started"
+
+const rootReducer = combineReducers({
+  get_started: get_started,
+  get_started: get_started,
+  get_started: get_started
+})
+
+export default rootReducer
